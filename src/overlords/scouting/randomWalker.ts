7f9756b8d2ab4f9e953b0cf271783fb08b150038
Etienne Samson <samson.etienne@gmail.com>
2023-08-13 07:35:35 +0200

    Logging for the changeling Overlord

--- src/overlords/scouting/randomWalker.ts
+++ src/overlords/scouting/randomWalker.ts
@@ -6,6 +6,7 @@ import {profile} from '../../profiler/decorator';
 import {Tasks} from '../../tasks/Tasks';
 import {Zerg} from '../../zerg/Zerg';
 import {Overlord} from '../Overlord';
+import { log } from 'console/log';
 
 const DEFAULT_NUM_SCOUTS = 3;
 
@@ -41,11 +42,13 @@ export class RandomWalkerScoutOverlord extends Overlord {
 		};
 
 		// Check if room might be connected to newbie/respawn zone
-		const indestructibleWalls = _.filter(scout.room.walls, wall => wall.hits == undefined);
-		if (indestructibleWalls.length > 0) { // go back to origin colony if you find a room near newbie zone
-			scout.task = Tasks.goToRoom(this.colony.room.name); // todo: make this more precise
-			return;
-		}
+		// const indestructibleWalls = _.filter(scout.room.walls, wall => wall.hits == undefined);
+		// if (indestructibleWalls.length > 0) { // go back to origin colony if you find a room near newbie zone
+		// 	this.debug(`${scout.print} detected indestructible walls, moving somewhere else`);
+		// 	log.info(`${scout.print} moving back to ${this.colony.print}`)
+		// 	scout.task = Tasks.goToRoom(this.colony.room.name, { moveOptions });
+		// 	return;
+		// }
 
 		const roomStatus = RoomIntel.getRoomStatus(scout.room.name);
 
@@ -53,20 +56,24 @@ export class RandomWalkerScoutOverlord extends Overlord {
 		neighboringRooms = _.shuffle(neighboringRooms);
 
 		// Pick a new random room from the neighboring rooms, making sure they have compatible room status
-		let neighboringRoom;
+		let neighboringRoom: string | undefined;
 		while ((neighboringRoom = neighboringRooms.shift())) {
 
 			const neighborStatus = RoomIntel.getRoomStatus(scout.room.name);
 			if (roomStatus.status !== neighborStatus.status) {
+				this.debug(`${scout.print} room ${neighboringRoom} status doesn't match ${scout.room.name}: ${roomStatus} ${neighborStatus.status}`);
 				continue;
 			}
 
+			log.info(`${scout.print} moving to ${neighboringRoom}`)
 			scout.task = Tasks.goToRoom(neighboringRoom, { moveOptions });
 			break;
 		}
 
 		// Just move back to the colony and start over
 		if (!scout.task) {
+			this.debug(`${scout.print} no task`);
+			log.info(`${scout.print} moving back to ${this.colony.print}`)
 			scout.task = Tasks.goToRoom(this.colony.room.name, { moveOptions });
 		}
 	}
