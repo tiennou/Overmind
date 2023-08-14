782c6c9ec512ef411d8046ccc1b998c268b4ea5e
Etienne Samson <samson.etienne@gmail.com>
2023-08-14 02:25:40 +0200

    Wire the NPC defenders to use the CombatCreep method
    I caught one case of a Broodling not healing itself properly and dying
    to a SK. Since that other method seems to handle everything already, and
    actually uses healing (almost) properly, switch to use that.
    
    "almost" because I saw it repeatedly heal a non-injured transporter that
    was around.

--- src/overlords/defense/npcDefense.ts
+++ src/overlords/defense/npcDefense.ts
@@ -34,49 +34,6 @@ export class DefenseNPCOverlord extends Overlord {
 	// 	this.guards = this.creeps('guard');
 	// }
 
-	private findAttackTarget(guard: CombatZerg): Creep | Structure | undefined | null {
-		const targetingDirectives = DirectiveTargetSiege.find(guard.room.flags) as DirectiveTargetSiege[];
-		const targetedStructures = _.compact(_.map(targetingDirectives,
-												   directive => directive.getTarget())) as Structure[];
-		if (targetedStructures.length > 0) {
-			return guard.pos.findClosestByRange(targetedStructures);
-		}
-		if (guard.room.hostiles.length > 0) {
-			const targets = _.filter(guard.room.hostiles, hostile => hostile.pos.rangeToEdge > 0);
-			return guard.pos.findClosestByRange(targets);
-		}
-		if (guard.room.hostileStructures.length > 0) {
-			const haulFlags = DirectiveHaul.find(guard.room.flags);
-			if (haulFlags.length == 0) {
-				return guard.pos.findClosestByRange(guard.room.hostileStructures);
-			}
-		}
-	}
-
-	/**
-	 * Attack and chase the specified target
-	 */
-	private combatActions(guard: CombatZerg, target: Creep | Structure): void {
-		// Attack the target if you can, else move to get in range
-		guard.attackAndChase(target);
-		// Heal yourself if it won't interfere with attacking
-		guard.healSelfIfPossible();
-	}
-
-	private handleGuard(guard: CombatZerg): void {
-		if (!guard.inSameRoomAs(this) || guard.pos.isEdge) {
-			// Move into the assigned room if there is a guard flag present
-			guard.goToRoom(this.pos.roomName);
-		} else { // If you're in the assigned room or if there is no assignment, try to attack or heal
-			const attackTarget = this.findAttackTarget(guard);
-			if (attackTarget) {
-				this.combatActions(guard, attackTarget);
-			} else {
-				guard.doMedicActions(this.pos.roomName);
-			}
-		}
-	}
-
 	init() {
 		const amount = this.room && (this.room.invaders.length > 0 || this.room.invaderCore || RoomIntel.isInvasionLikely(this.room)) ? 1 : 0;
 		let setup = CombatSetups.broodlings.default;
@@ -92,7 +49,7 @@ export class DefenseNPCOverlord extends Overlord {
 			if (guard.hasValidTask) {
 				guard.run();
 			} else {
-				this.handleGuard(guard);
+				guard.autoCombat(this.pos.roomName, true, undefined, {});
 			}
 		}
 	}
