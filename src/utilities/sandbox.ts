b156a5b2cb41f77fe3e90bc730990d1f3ae5bbca
Etienne Samson <samson.etienne@gmail.com>
2023-09-02 20:42:57 +0200

    Disable sandbox testing code

--- src/utilities/sandbox.ts
+++ src/utilities/sandbox.ts
@@ -1,18 +1,5 @@
 // Sandbox code: lets you try out random stuff at the end of main loop
 
-import {log} from '../console/log';
-import {PackratTests} from './packrat';
-
-// import {tftest} from './reinforcementLearning/test'
-
 export function sandbox() {
-	try {
-		global.PackratTests = PackratTests;
-	} catch (e) {
-		if (e instanceof Error) {
-			log.error(e);
-		} else {
-			console.log(e);
-		}
-	}
+
 }
